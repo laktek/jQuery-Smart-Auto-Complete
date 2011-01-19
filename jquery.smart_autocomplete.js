777d7d04dc0015cb51b1a4f53dc5f4929efddf14
Lakshan Perera <lakshanperera@lakshan-pereras-macbook.local>
2011-01-19 23:51:23 +0530

    basic specs for the plugin

--- jquery.smart_autocomplete.js
+++ jquery.smart_autocomplete.js
@@ -9,12 +9,13 @@
 /*
  $(target).smartAutoComplete({options})
   options:
-  minCharLength: (integer) minimum characters user have to type before invoking the autocomplete (default: 2)
+  minCharLimit: (integer) minimum characters user have to type before invoking the autocomplete (default: 2)
   maxResults: (integer) maximum number of results to return (default: -1 (unlimited))
-  delay: (integer) delay before autocomplete starts (default: 10ms)
+  delay: (integer) delay before autocomplete starts (default: 300ms)
   typeAhead: (boolean) fill the field with the best matching result, as in google instant search (default: false)
              and fires the select event.
   disabled: (boolean) whether autocomplete disabled on the field (default: false)
+  forceSelect: (boolean) always fills the field with best matching result, without leaving custom input (similar to a select field) (default false)
   source:  (array/function) you can supply an array or callback function that would return an array for the source
            this is optional if you prefer to have your own filter method 
            eg: ["Apple", "Banana", "Mango"] or [["Apple", 1], ["Banana", 2], ["Mango", 3]]
@@ -26,16 +27,42 @@
                    expects to return a string
                    arguments: result 
   appendTo: (selector) to which elements the result should be appended.
-  forceSelect: (boolean) always fills the field with best matching result, without leaving custom input (similar to a select field) (default false)
 
   events:
-  select: fires when user selects an item from the result list
-  enable: fires when autocomplete is disabled
-  disable: fires when autocomplete is enabled
+  keyIn: fires when user types into the field
   resultsReady: fires when filtered results are appended to the view 
   noMatch: fires when filter returns an empty array to append to the view
-
-
+  itemHover: fires when user hovers over an item in the result list
+  itemSelect: fires when user selects an item from the result list
  })
 */
+
+(function($){
+  $.fn.smartAutoComplete = function(){    
+
+    var default_options = { minCharLimit: 2, maxResults: -1, delay: 300, typeAhead: false, disabled: false, forceSelect: false };
+    var passed_options = arguments[0] || {};
+
+    return this.each(function(i) { 
+      //set the options
+      var options = $.extend(default_options, $(this).data("smart-autocomplete"), passed_options);
+      initialized_options = options;
+      $(this).data("smart-autocomplete", options);
+
+      //bind the events
+      $(this).bind('keyIn.smart_autocomplete', function(ev, query){
+        var options = $(this).data("smart-autocomplete");
+        var source = options.source || null;
+
+        if(options.disabled)
+          return false;
+
+        //call the filter function
+        options.filter.call(this, query, options.source)
+
+      });
+
+    });
+  }
+})(jQuery);
 
---
+++ spec/core/jquery.smart_autocomplete_spec.js
@@ -0,0 +1,114 @@
+describe('Smart AutoComplete', function () {
+    
+    beforeEach(function () {
+       setFixtures("<input id='autoCompleteField'/>");
+    });
+
+    describe('initializing with default values', function(){
+
+      var smart_autocomplete_options;
+
+      beforeEach(function(){
+        $("#autoCompleteField").smartAutoComplete();
+        smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+      });
+
+      it('minimum character length should be 2', function () {
+        expect(smart_autocomplete_options.minCharLimit).toEqual(2);
+      });
+
+      it('maximum results should be unlimited', function () {
+        expect(smart_autocomplete_options.maxResults).toEqual(-1);
+      });
+
+      it('delay should be 300ms', function () {
+        expect(smart_autocomplete_options.delay).toEqual(300);
+      });
+
+      it('type ahead should be false', function () {
+        expect(smart_autocomplete_options.typeAhead).toEqual(false);
+      });
+
+      it('disabled should be false', function () {
+        expect(smart_autocomplete_options.disabled).toEqual(false);
+      });
+
+      it('force select should be false', function () {
+        expect(smart_autocomplete_options.forceSelect).toEqual(false);
+      });
+
+    });
+
+    describe('overriding default values', function(){
+
+      it('takes hardcoded options', function(){
+       setFixtures("<input id='autoCompleteFieldHardCoded' data-smart-autocomplete='{\"minCharLimit\": 4 }'/>");
+       $("#autoCompleteFieldHardCoded").smartAutoComplete();
+       smart_autocomplete_options = $("#autoCompleteFieldHardCoded").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.minCharLimit).toEqual(4);
+      });
+
+      it('minimum character limit set to 4', function () {
+       $("#autoCompleteField").smartAutoComplete({minCharLimit: 4});
+       smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.minCharLimit).toEqual(4);
+      });
+
+      it('maximum results set to 10', function () {
+       $("#autoCompleteField").smartAutoComplete({maxResults: 10});
+       smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.maxResults).toEqual(10);
+      });
+
+      it('delay set to 10ms', function () {
+       $("#autoCompleteField").smartAutoComplete({delay: 10});
+       smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.delay).toEqual(10);
+      });
+
+      it('type ahead set to true', function () {
+       $("#autoCompleteField").smartAutoComplete({typeAhead: true});
+       smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.typeAhead).toEqual(true);
+      });
+
+      it('disabled set to true', function () {
+       $("#autoCompleteField").smartAutoComplete({disabled: true});
+       smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.disabled).toEqual(true);
+      });
+
+      it('force select set to true', function () {
+       $("#autoCompleteField").smartAutoComplete({forceSelect: true});
+       smart_autocomplete_options = $("#autoCompleteField").data("smart-autocomplete");
+
+       expect(smart_autocomplete_options.forceSelect).toEqual(true);
+      });
+
+    });
+
+    describe('key in event', function(){
+
+      it("performs no action if disabled", function(){
+        $("#autoCompleteField").smartAutoComplete({disabled: true});
+        //expect($("#autoCompleteField").trigger("keyIn")).toBeFalsy();
+      });
+      
+      it("calls the filter function with query and source", function(){
+        var output_buffer;
+        $("#autoCompleteField").smartAutoComplete({filter: function(q, s){ output_buffer = "received " + q + " & " + s; }, source: "test"});
+        $("#autoCompleteField").trigger("keyIn", "t");
+        expect(output_buffer).toEqual("received t & test");
+      });
+
+    
+    });
+
+});
+
